e5ee3023db3c9f008f5cd5564be7d542de6f35a8
Deyson A. Venté <deiandresvent@hotmail.com>
2021-10-20 12:10:14 -0500

    primera subida

---
+++ src/Clases/Conectar.java
@@ -0,0 +1,23 @@
+
+package Clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conectar {
+    
+    Connection cn;
+    
+    public Connection conexion(){
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cn = DriverManager.getConnection("jdbc:mysql://localhost/empleados","root","");
+            System.out.println("Conectado");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return cn;
+    }
+    
+}
